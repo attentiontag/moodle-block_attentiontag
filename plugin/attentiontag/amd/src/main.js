dedda057990ddf87eeed6df55781eac479a84733
Smruti-AT <smruti@attentiontag.com>
2024-12-03 00:42:56 +0530

    user and course mapping

--- plugin/attentiontag/amd/src/main.js
+++ plugin/attentiontag/amd/src/main.js
@@ -1,29 +1,52 @@
-define(['@attention_tag/attentiontag'], function(atSDK) {
-    'use strict';
-
-    function init() {
-        const visualPromptContainer = document.getElementById('attentiontag-container')
+define(["@attention_tag/attentiontag"], function(atSDK) {
+    'use strict'
+    function init(props) {
+        const visualPromptContainer = document.getElementById("attentiontag-visual-prompt-container")
+        const auditoryCueContainer = document.getElementById("attentiontag--auditory-cue-container")
+        const chatWidgetContainer = document.getElementById("attentiontag-chat-widget-container")
+        // const viewerDetails = {
+        //     id: 229,
+        //     email: "shahidracer3@gmail.com"
+        // }
+        // const meetingDetails = {
+        //     id: 1338,
+        //     project: 162,
+        //     lesson: 583,
+        //     content: 1061,
+        //     start_datetime: new Date(),
+        //     duration_sec: 99999999
+        // }
         const viewerDetails = {
-            id: 386,
-            email: 'smruti_viewer@attentiontag.com'
+            id: 409,
+            email: "new_sdk_viewer@gmail.com"
         }
         const meetingDetails = {
-            id: 1338,
-            project: 162,
-            lesson: 583,
-            content: 1061,
-            start_datetime: new Date('28 Nov 2024'),
-            duration_sec: 999999
+            id: 716,
+            project: 81,
+            module_id: 83,
+            lesson: 94,
+            content: 393,
+            start_datetime: new Date(),
+            duration_sec: 99999999
         }
+
         const at = new atSDK.AttentionTag(null, viewerDetails, viewerDetails.email, meetingDetails, meetingDetails.project, null, null);
         at.init();
-        at.startObservation(true, meetingDetails.start_datetime, new Date('29 Nov 2024'))
+        at.startMeeting(meetingDetails);
+        at.startObservation(true, meetingDetails.start_datetime, new Date("30 Nov 2025"))
         at.startLiveWorkflows()
         if(visualPromptContainer) {
             at.initVisualPrompt(visualPromptContainer)
         }
-        console.log('AT object', at)
-        console.log('AttentionTag React component initialized.');
+        if(auditoryCueContainer) {
+            at.initAuditoryCue(auditoryCueContainer)
+        }
+        if(chatWidgetContainer) {
+            at.initChatWidget(chatWidgetContainer)
+        }
+        console.log("AT object", at)
+        console.log("AttentionTag React component initialized.");
+        console.log("Props", props)
     }
 
     return {
--- plugin/attentiontag/block_attentiontag.php
+++ plugin/attentiontag/block_attentiontag.php
@@ -17,10 +17,18 @@ class block_attentiontag extends block_base {
     }
 
     public function get_required_javascript() {
-        global $PAGE, $OUTPUT;
+        global $PAGE, $OUTPUT, $USER, $LESSON, $COURSE;
 
         // Generate the HTML for the floating icon using the Mustache template.
         $icon_html = json_encode($OUTPUT->render_from_template('block_attentiontag/content', []));
+        $user = json_encode($USER);
+
+        // below line gives unknown property of PAGE error
+        // $lesson = json_encode($PAGE->lesson);
+
+        $lesson = $LESSON;
+        $page_course = json_encode($PAGE->course);
+        $course = json_encode($COURSE);
 
         // Inject JavaScript to add the floating icon to the footer.
         $js_code = <<<JS
@@ -33,7 +41,7 @@ class block_attentiontag extends block_base {
                     floatingIcon.init(); // Initialize the floating icon module.
 
                     // Initialize the attentiontag SDK.
-                    main.init();
+                    main.init({user: $user, lesson: $lesson, page_course: $page_course, course: $course});
                 });
             });
 JS;
